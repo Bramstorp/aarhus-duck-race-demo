9a4e5f72bbc3551d43bc5ff8e5114b27f35118fe
Bramstorp <m.bramstorp@stofanet.dk>
2021-12-07 18:05:01 +0100

    added BrowserRouter to index + 404 / index.html script

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import { NotFound } from "./NotFound";
 import { Layout } from "./Layout/Layout";
@@ -13,19 +13,17 @@ import { Purpose } from "./modules/Purpose/Purpose"
 
 function App() {
   return (
-    <HashRouter basename={"/aarhus-duck-race-demo"}>
       <Layout>
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/concept/" component={Concept} />
-          <Route exact path="/buy/" component={Buy} />
-          <Route exact path="/event/" component={Event} />
-          <Route exact path="/prizes/" component={Prizes} />
-          <Route exact path="/purpose/" component={Purpose} />
+          <Route exact path="/concept" component={Concept} />
+          <Route exact path="/buy" component={Buy} />
+          <Route exact path="/event" component={Event} />
+          <Route exact path="/prizes" component={Prizes} />
+          <Route exact path="/purpose" component={Purpose} />
           <Route component={NotFound} />
         </Switch>
       </Layout>
-    </HashRouter>
   );
 }
 
